9943506aee6760d7858f9e3c4667a267dbab4b05
Augusto Xavier <augustocbx@gmail.com>
2018-10-15 07:10:45 -0300

    Fix tika/tesseract

--- app/models/document.rb
+++ app/models/document.rb
@@ -27,7 +27,7 @@ class Document < ApplicationRecord
     # puts `ls -lah #{self.document.queued_for_write[:original].path}`
     # p self.document.methods
     # puts "java -cp #{jars_path}/jai_core-1.1.3.jar:#{jars_path}/jai_imageio.jar:#{jars_path}/jbig2_1.4.jar:#{jars_path}/levigo-jbig2-imageio-1.6.1.jar:#{jars_path}/tika-parsers-1.13.jar -jar bin/tika-app-1.12.jar --text-main #{self.document.queued_for_write[:original].path}"
-    self.content = `java -cp #{jars_path}/jai_core-1.1.3.jar:#{jars_path}/jai_imageio.jar:#{jars_path}/jbig2_1.4.jar:#{jars_path}/levigo-jbig2-imageio-1.6.1.jar:#{jars_path}/tika-parsers-1.13.jar -jar bin/tika-app-1.12.jar --text-main #{self.document.queued_for_write[:original].path}`[0..33000]
+    self.content = `java -cp #{jars_path}/jai_core-1.1.3.jar:#{jars_path}/jai_imageio.jar:#{jars_path}/jbig2_1.4.jar:#{jars_path}/levigo-jbig2-imageio-1.6.1.jar:#{jars_path}/tika-parsers-1.13.jar -jar bin/tika-app-1.12.jar --text-main file://#{self.document.queued_for_write[:original].path}`[0..33000]
     if self.content.empty?
       self.content = `tesseract #{self.document.queued_for_write[:original].path} stdout`
     end
